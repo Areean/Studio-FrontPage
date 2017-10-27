0b68f5323d0b0df317b831effa1bc21a8a22650b
AmirTavakoli <amirtavakoli@outlook.com>
2017-10-27 12:41:27 +0330

    listening app

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let express=require('express');
 
 //creatign an express app 
 var app =express();
+app.listen(3000, function() {
+  console.log('server is listenening on port 3000');
+});
 
 app.get('*', function( req, res ) {
     res.send('Hello World!');
