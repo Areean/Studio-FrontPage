ac4e26ff82ab76cf264f7e7cb389063bd0a5b538
AmirTavakoli <amirtavakoli@outlook.com>
2017-10-27 21:12:41 +0330

    added web.config

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-const app = require('http');
-
-app.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('<H1>Hello World!</H1>');
-  
-}).listen(process.env.PORT);
-
-
-//let express=require('express'),
-//path=require('path'),
-//bodyParser=require('body-parser');
-//creatign an express app 
-
-//let app =express();
-
-
-// parse application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-//app.use(bodyParser.json())
+var express=require('express'),
+compression = require('compression'),
+path=require('path'),
+bodyParser=require('body-parser'),
+http = require('http');
 
 var options = {
   dotfiles: 'ignore',
@@ -32,30 +16,26 @@ var options = {
   }
 }
 
+var app = express();
+var server = http.createServer(app);
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+// use public folder
 app.use(express.static('public', options))
-
-
-
-
 app.use('/', express.static('app', options));
 
-let server = app.listen(8081, function () {
-  let host = server.address().address;
-  let port = server.address().port;
-  
-  console.log('my app is listening at http://%s:%s', host, port);
+server.listen(3000, 'localhost');
+server.on('listening', function() {
+    console.log('Express server started on port %s at %s', server.address().port, server.address().address);
 });
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
-  
-});
-
-router.get('/', function(req, res){
-  res.render('index', {
-    title: 'Home'
   });
-});
+
 app.get('*', function(req, res) {
   res.writeHead(302, {
     'Location': '/'
@@ -63,3 +43,17 @@ app.get('*', function(req, res) {
   //res.redirect('your/404/path.html');
   res.end();
 });
+
+
+/*
+*
+*
+//app--with--http--request
+const app = require('http');
+app.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('<H1>Hello World!</H1>');
+  }).listen(process.env.PORT);
+*/
+
+
