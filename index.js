cd2d96992e4688522c997894ca7c63ee2c3b8926
AmirTavakoli <amirtavakoli@outlook.com>
2017-10-27 12:57:07 +0330

    error fixed

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ let express=require('express');
 //creatign an express app 
 var app =express();
 app.listen(7000, function() {
-  console.log('server is listenening on port 3000');
+  console.log('server is listenening on port 7000');
 });
 
 app.get('/', function( req, res ) {
-    res.send('Hello World!');
-  });
+  res.sendFile('index.html', { root: './'});
+ // next();
+});
+
+app.get('*', function( req, res ) {
+  res.send('Hello World!');
+});
+
